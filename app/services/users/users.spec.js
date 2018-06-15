fea3858371ada8d6429bfaea44440cbd0ba93d91
Daniel Lynn <daniel.lynn@agile42.com>
2018-06-14 18:09:46 -0700

    First commit

---
+++ app/services/users/users.spec.js
@@ -0,0 +1,63 @@
+describe('Users factory', function() {
+	var Users;
+
+	var userList = [
+	    {
+	      id: '1',
+	      name: 'Jane',
+	      role: 'Designer',
+	      location: 'New York',
+	      twitter: 'gijane'
+	    },
+	    {
+	      id: '2',
+	      name: 'Bob',
+	      role: 'Developer',
+	      location: 'New York',
+	      twitter: 'billybob'
+	    },
+	    {
+	      id: '3',
+	      name: 'Jim',
+	      role: 'Developer',
+	      location: 'Chicago',
+	      twitter: 'jimbo'
+	    },
+	    {
+	      id: '4',
+	      name: 'Bill',
+	      role: 'Designer',
+	      location: 'LA',
+	      twitter: 'dabill'
+	    }
+	  ];
+
+	beforeEach(angular.mock.module('api.users'));
+
+	beforeEach(inject(function(_Users_) {
+
+		Users = _Users_;
+
+	}));
+
+	it('should exist', function() {
+		expect(Users).toBeDefined(); 
+	});
+
+	// A set of tests for our Users.all() method
+  	describe('.all()', function() {
+    	// A simple test to verify the method all exists
+    	it('should exist', function() {
+      		expect(Users.all).toBeDefined();
+    	});
+
+    	it('should return a hard-coded user list', function() {
+    		expect(Users.all()).toEqual(userList);
+    	});
+
+    	it('should fail', function() {
+    		expect().toEqual(true);
+    	});
+  	});
+
+});
